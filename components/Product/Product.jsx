e2e2dc417fab75c6b151b6bdf201c02e6a796f02
JoshHogan99 <52086873+JoshHogan99@users.noreply.github.com>
2024-03-20 00:54:45 +0000

    Added JS & CSS

--- components/Product/Product.jsx
+++ components/Product/Product.jsx
@@ -3,6 +3,8 @@ import {NavLink, useParams} from "react-router-dom"
 
 import {getProduct} from "../../api"
 import ProductBtn from "../ProductBtn/ProductBtn"
+import Category from "../../components/Category/Category.jsx"
+import BestGear from "../../components/BestGear/BestGear.jsx"
 
 import "./Product.css"
 
@@ -47,14 +49,51 @@ export default function Product(){
 
     const includesElement = product ? product.includes.map(includesInfo => {
         return(
-            <div className="selected-products-includes-info">
-                <p className="selected-products-includes-quantity">
+            <div className="selected-product-includes-info">
+                <p className="selected-product-includes-quantity">
                     {includesInfo.quantity}x
                 </p>
 
-                <p className="selected-products-includes-item">
+                <p className="selected-product-includes-item">
                     {includesInfo.item}
                 </p>
+            </div>
+        )
+    }) : null
+
+    const formattedFeatures = product ? product.features.replace(/\.([^ ])/g, '.<br><br>$1') : null
+
+    const othersElement = product ? product.others.map(othersInfo => {
+        return(
+            <div className="selected-product-others-info">
+                <img 
+                    src={`../.${othersInfo.image.mobile}`} 
+                    alt={othersInfo.slug}
+                    className="mobile" 
+                />
+
+                <img 
+                    src={`../.${othersInfo.image.tablet}`} 
+                    alt={othersInfo.slug}
+                    className="tablet" 
+                />
+
+                <img 
+                    src={`../.${othersInfo.image.desktop}`} 
+                    alt={othersInfo.slug}
+                    className="desktop" 
+                />
+
+                <p className="selected-product-others-name">
+                    {othersInfo.name}
+                </p>
+
+                <ProductBtn 
+                    background="#D87D4A"
+                    color="#FFF"
+                    border="none"
+                    text="see product"
+                />
             </div>
         )
     }) : null
@@ -64,6 +103,7 @@ export default function Product(){
             <NavLink
                 to={`..`}
                 relative="path"
+                className="go-back-btn"
             >
                 Go Back
             </NavLink>
@@ -98,19 +138,21 @@ export default function Product(){
                         <p className="selected-product-price">$ {product.price.toLocaleString()}</p>
             
                         <div className="quantity-container">
-                            <button 
-                                onClick={subtractQuantity}
-                                disabled={quantity === 1}
-                            >
-                                -
-                            </button>
-                            {quantity}
-                            <button 
-                                onClick={addQuantity}
-                                disabled={quantity === 10}
-                            >
-                                +
-                            </button>
+                            <div className="quantity-button">
+                                <button 
+                                    onClick={subtractQuantity}
+                                    disabled={quantity === 1}
+                                >
+                                    -
+                                </button>
+                                <p>{quantity}</p>
+                                <button 
+                                    onClick={addQuantity}
+                                    disabled={quantity === 10}
+                                >
+                                    +
+                                </button>
+                            </div>
 
                             <ProductBtn 
                                 background="#D87D4A"
@@ -123,19 +165,93 @@ export default function Product(){
                         <div className="selected-product-features-container">
                             <p className="selected-product-features-title">FEATURES</p>
 
-                            <p className="selected-product-features-info"> 
-                                {product.features}
+                            <p 
+                                className="selected-product-features-info" 
+                                dangerouslySetInnerHTML={{__html: formattedFeatures}}
+                            >
                             </p>
                         </div>
 
                         <div className="selected-product-includes-container">
                             <p className="selected-product-includes-title">IN THE BOX</p>
 
-                            {includesElement}
+                            <div className="selected-product-includes-info-container">
+                                {includesElement} 
+                            </div>
+                        </div>
+
+                        <div className="selected-product-gallery-container">
+                            <img 
+                                src={`../.${product.gallery.first.mobile}`} 
+                                alt="Gallery Logo One"
+                                className="mobile" 
+                            />
+
+                            <img 
+                                src={`../.${product.gallery.first.tablet}`} 
+                                alt="Gallery Logo One"
+                                className="tablet" 
+                            />
+
+                            <img 
+                                src={`../.${product.gallery.first.desktop}`} 
+                                alt="Gallery Logo One"
+                                className="desktop" 
+                            />
+
+                            <img 
+                                src={`../.${product.gallery.second.mobile}`} 
+                                alt="Gallery Logo Two"
+                                className="mobile" 
+                            />
+
+                            <img 
+                                src={`../.${product.gallery.second.tablet}`} 
+                                alt="Gallery Logo Two"
+                                className="tablet" 
+                            />
+
+                            <img 
+                                src={`../.${product.gallery.second.desktop}`} 
+                                alt="Gallery Logo Two"
+                                className="desktop" 
+                            />
+
+                            <img 
+                                src={`../.${product.gallery.third.mobile}`} 
+                                alt="Gallery Logo Three"
+                                className="mobile" 
+                            />
+
+                            <img 
+                                src={`../.${product.gallery.third.tablet}`} 
+                                alt="Gallery Logo Three"
+                                className="tablet" 
+                            />
+
+                            <img 
+                                src={`../.${product.gallery.third.desktop}`} 
+                                alt="Gallery Logo Three"
+                                className="desktop" 
+                            />
+                        </div>
+
+                        <div className="selected-product-others-container">
+                            <p className="selected-product-others">
+                                YOU MAY ALSO LIKE
+                            </p>
+
+                            <div className="selected-product-others-info-container">
+                                {othersElement}
+                            </div>
                         </div>
                     </div>
                 </div>
             )}
+            
+            <Category />
+
+            <BestGear />
         </div>
     )
 }
